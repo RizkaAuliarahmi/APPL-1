6b213c714c756f287cc11595b1256cd4dfd7aabb
RizkaAuliarahmi <58288894+RizkaAuliarahmi@users.noreply.github.com>
2021-03-28 02:49:29 +0700

    Add files via upload

--- W2/ACurrencyConverter/RatePanel.java
+++ W2/ACurrencyConverter/RatePanel.java
@@ -24,6 +24,7 @@ public class RatePanel extends JPanel
  private String[] currencyName; 
  private JLabel result; 
  private JComboBox box;
+ JTextField text;
  // ------------------------------------------------------------ 
  // Sets up a panel to convert cost from one of 6 currencies 
  // into U.S. Dollars. The panel contains a heading, a text 
@@ -45,20 +46,26 @@ public class RatePanel extends JPanel
  0.0222, 0.0880}; 
  
  result = new JLabel (" ------------ "); 
- add (title); 
- 
- add (result); 
- 
+ JPanel pane1 = new JPanel();
+ pane1.add (title); 
+ pane1.add (result); 
+ add(pane1,"north");
  //combo box for currency
  JComboBox box = new JComboBox(currencyName);
  box.addActionListener(new ComboListener());
- add(box);
  
+ 
+ text = new JTextField(10);
+ text.addActionListener(new ComboListener());
  //cost user input
  JLabel cost = new JLabel();
  String c = cost.getText();
  System.out.println(c);
- add(cost);
+ JPanel pane2 = new JPanel();
+ pane2.add(box);
+ pane2.add(cost);
+ pane2.add(text);
+ add(pane2,"south");
  } 
  // ****************************************************** 
  // Represents an action listener for the combo box. 
@@ -73,10 +80,11 @@ public class RatePanel extends JPanel
  public void actionPerformed (ActionEvent event) 
  { 
      JComboBox box = (JComboBox)event.getSource();
+     double cst = Double.parseDouble(text.getText());
      int index = (int)box.getSelectedIndex(); 
      try{
-    result.setText ("1 " + currencyName[index] +  " = " + 
-         rate[index] + " U.S. Dollars"); 
+    result.setText (cst + currencyName[index] +  " = " + 
+         rate[index]*cst + " U.S. Dollars"); 
      }catch(ArrayIndexOutOfBoundsException e){
     System.out.println("whyyy");}
  } 
